0823ca8cf524f82c24204985e618508b888f4515
benjavrcc <bvillalobos@estudiante.uc.cl>
2025-11-23 04:11:32 -0300

    Agrego cantidad_viajeros al modelo Registro

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class Registro(BaseModel):
     fecha_llegada: str
     hora_llegada: str
+    cantidad_viajeros: int
 
 class DistribucionRequest(BaseModel):
     registros: list[Registro]
